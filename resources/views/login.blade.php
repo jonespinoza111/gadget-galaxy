7e3abc328e0682bb8c40a21dcba182d1cbc269ad
jonespinoza111 <jonespinoza111@gmail.com>
2023-09-27 14:43:29 -0400

    login changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,12 +13,8 @@
                 <div class="md:mx-6 md:p-12">
                   <!--Logo-->
                   <div class="text-center">
-                    <img
-                      class="mx-auto w-48"
-                      src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                      alt="logo" />
                     <h4 class="mb-12 mt-1 pb-1 text-xl font-semibold">
-                      We are The Lotus Team
+                      We are GadgetGalaxy!
                     </h4>
                   </div>
   
@@ -73,13 +69,13 @@
                     <!--Register button-->
                     <div class="flex items-center justify-between pb-6">
                       <p class="mb-0 mr-2">Don't have an account?</p>
-                      <button
-                        type="button"
+                      <a
+                        href="/register"
                         class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                         data-te-ripple-init
                         data-te-ripple-color="light">
                         Register
-                      </button>
+                      </a>
                     </div>
                   </form>
                 </div>
