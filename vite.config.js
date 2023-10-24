58bef5161a7af02e80d9b4de22f70bbe17b5b75c
Jonathan <jonespinoza111@gmail.com>
2023-10-24 11:56:05 -0400

    assets

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        assetsInlineLimit: 0,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
