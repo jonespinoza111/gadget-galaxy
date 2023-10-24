1877cdf71f4619cde076b2ec17fa3f39503bc3a2
Jonathan <jonespinoza111@gmail.com>
2023-10-24 09:00:58 -0400

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./resources/**/*.js",
     "./resources/**/*.vue",
     "./node_modules/flowbite/**/*.js",
+    "./storage/framework/views/*.php"
   ],
   theme: {
     extend: {},
